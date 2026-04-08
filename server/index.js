ed6eec229d4a02113900d0fa308acac9452a4630
Chaiti487 <chaitisen487@gmail.com>
2026-04-09 00:27:50 +0530

    fix: Update CORS configuration to allow multiple origins

--- server/index.js
+++ server/index.js
@@ -9,12 +9,13 @@ import userRouter from "./routes/user.route.js";
 import notesRouter from "./routes/generate.route.js";
 
 const app = express()
-app.use(cors(
-  { origin: "http://localhost:5173",
-    credentials: true ,
-    methods:["GET","POST","PUT","DELETE","OPTIONS"]
-   }
-  ));
+app.use(
+  cors({
+    origin: ["https://exam-notes-ai-dusky.vercel.app" , "http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  }),
+);
 app.use(express.json())
 app.use(cookieParser())
 const PORT = process.env.PORT || 5000
